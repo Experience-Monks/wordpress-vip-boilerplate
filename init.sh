7eea3239436e4482fc4bcba0e602c2aef10168a7
Rodrigo Techera <rodrigo.techera@jam3.com>
2019-02-12 11:45:07 -0300

    changing some orders for init.sh script

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@ if $(wp core is-installed); then
     echo "wp installed"
     #returning 0 value marks success
     return 0
+else
+    wp core install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 fi
-
-wp core install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
 #returning non 0 value marks an error, so the container will start again as it has "restart: on-failure"
 return 1

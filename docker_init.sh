de130261708403624ad8ff2612d6eab1d51f7523
Rodrigo Techera <rodrigo.techera@jam3.com>
2019-04-08 17:30:04 -0300

    changing admin_password variable for docker_init.sh

--- docker_init.sh
+++ docker_init.sh
@@ -12,14 +12,14 @@ else
         if $WP_IS_MULTISITE_SUBDOMAIN
         then
             echo "installing wp multisite using subdomains..."
-            wp core multisite-install --url=$LOCAL_DEV_DOMAIN --subdomains --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+            wp core multisite-install --url="$LOCAL_DEV_DOMAIN" --subdomains --title="$WP_SITE_TITLE" --admin_name="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
         else
             echo "installing wp multisite using directories..."
-            wp core multisite-install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+            wp core multisite-install --url="$LOCAL_DEV_DOMAIN" --title="$WP_SITE_TITLE" --admin_name="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
         fi
     else
         echo "installing wp..."
-        wp core install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+        wp core install --url="$LOCAL_DEV_DOMAIN" --title="$WP_SITE_TITLE" --admin_name="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
     fi
 fi
 

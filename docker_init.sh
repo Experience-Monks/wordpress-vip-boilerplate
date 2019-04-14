9a6920a7c507626e0b04bbc8874fc91be540ba83
Rodrigo Techera <rodrigo.techera@jam3.com>
2019-04-14 19:04:58 -0300

    fixing theme installation

--- docker_init.sh
+++ docker_init.sh
@@ -2,24 +2,21 @@
 
 echo "initializing..."
 #installing wp
-if $(wp core is-installed); then
-    echo "wp installed"
-
-    if [ -n "$WP_PROJECT_THEME_FOLDER" ];
-    then
-        echo "mkdir wp-content/themes/$WP_PROJECT_THEME_FOLDER"
-        mkdir wp-content/themes/$WP_PROJECT_THEME_FOLDER
-    fi
-
+if $(wp core is-installed);
+then
+    echo "wp already installed"
+    
     if $WP_IS_MULTISITE
     then
         if $WP_IS_MULTISITE_SUBDOMAIN
         then
-            echo "installing wp multisite using subdomains..."
-            wp core multisite-install --url=$LOCAL_DEV_DOMAIN --subdomains --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+            echo "converting to wp multisite using subdomains..."
+            #wp core multisite-install --url=$LOCAL_DEV_DOMAIN --subdomains --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+            wp core multisite-convert --title="$WP_SITE_TITLE" --subdomains
         else
-            echo "installing wp multisite using directories..."
-            wp core multisite-install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+            echo "converting to wp multisite using directories..."
+            #wp core multisite-install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+            wp core multisite-convert --title="$WP_SITE_TITLE" --subdomains
         fi
 
         if [ -n "$WP_PROJECT_THEME_FOLDER" ];
@@ -27,16 +24,26 @@ if $(wp core is-installed); then
             echo "wp theme enable $WP_PROJECT_THEME_FOLDER --network"
             wp theme enable $WP_PROJECT_THEME_FOLDER --network
         fi
-    else
-        echo "wp theme enable $WP_PROJECT_THEME_FOLDER"
-        wp theme enable $WP_PROJECT_THEME_FOLDER
     fi
 
     #returning 0 value marks success
     exit 0
 else
     echo "installing wp..."
-    wp core install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL 
+    wp core install --url=$LOCAL_DEV_DOMAIN --title="$WP_SITE_TITLE" --admin_name=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+    if [ -n "$WP_PROJECT_THEME_FOLDER" ];
+    then
+        echo "mkdir wp-content/themes/$WP_PROJECT_THEME_FOLDER"
+        mkdir wp-content/themes/$WP_PROJECT_THEME_FOLDER
+        echo "touch wp-content/themes/$WP_PROJECT_THEME_FOLDER/style.css"
+        touch wp-content/themes/$WP_PROJECT_THEME_FOLDER/style.css
+        echo "touch wp-content/themes/$WP_PROJECT_THEME_FOLDER/index.php"
+        touch wp-content/themes/$WP_PROJECT_THEME_FOLDER/index.php
+
+        echo "wp theme activate $WP_PROJECT_THEME_FOLDER"
+        wp theme activate $WP_PROJECT_THEME_FOLDER
+    fi
 fi
 
 #returning non 0 value marks an error, so the container will start again as it has "restart: on-failure"
